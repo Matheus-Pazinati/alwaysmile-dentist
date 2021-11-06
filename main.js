3171fc17d3c52af48c7293b0cc5daf7f732ed592
Matheus Pazinati <matheuspazinati@gmail.com>
2021-11-06 08:39:59 -0300

    Aplica responsividade para dispositivos pequenos ate 480px

--- main.js
+++ main.js
@@ -11,29 +11,37 @@ window.addEventListener ('scroll', () => {
 
 //Criando carrossel dos depoimentos com o Swiper
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 2,
-  slidesPerGroup: 2,
+
+  slidesPerView: 1,
   spaceBetween: 32,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  keyboard: true
+  keyboard: true,
+    breakpoints: {
+    481: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      mousewheel: true,
+    },
+  },
 });
 
 //Adicionando efeito de "lentidão" ao dar scroll na página utilizando ScrollReveal
-const scrollReveal = ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  duration: 700,
-  reset: true,
-})
+// const scrollReveal = ScrollReveal({
+//   origin: 'top',
+//   distance: '30px',
+//   duration: 700,
+//   reset: true,
+// })
 
-scrollReveal.reveal(
-  `#main .description, #main .clinic,
-   #about .about-image, #about .about-us,
-   #services .services-description, #services .services-examples,
-   #testimony header, #testimony .testimony-items,
-   #contact .contact-text, #contact .contact-information`
-   , { interval: 100 }
-  )
+// scrollReveal.reveal(
+//   `#main .description, #main .clinic,
+//    #about .about-image, #about .about-us,
+//    #services .services-description, #services .services-examples,
+//    #testimony header, #testimony .testimony-items,
+//    #contact .contact-text, #contact .contact-information,
+//    footer .copyright, footer .socials`
+//    , { interval: 100 }
+//   )
